195ab5132e3ca5d7f7c69636f30ea28122286860
Taras <117749635+vasylkivt@users.noreply.github.com>
2023-06-20 21:27:44 +0300

    add jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import user from '../bd/userProfile/user.json';
+import Profile from './userProfile/userProfile';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      React homework 01
-    </div>
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stats.followers}
+        views={user.stats.views}
+        likes={user.stats.likes}
+      />
+    </>
   );
 };
