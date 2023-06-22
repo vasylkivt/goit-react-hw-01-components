8affdb5ff9a8af2be29a10fb893e0e5902bdb0d6
Taras <117749635+vasylkivt@users.noreply.github.com>
2023-06-22 12:37:17 +0300

    Update userProfile.js

--- src/components/userProfile/userProfile.js
+++ src/components/userProfile/userProfile.js
@@ -39,13 +39,13 @@ export default function Profile({
 Profile.propTypes = {
   user: PropTypes.shape({
     username: PropTypes.string,
-    tag: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
+    tag: PropTypes.string,
+    location: PropTypes.string,
+    avatar: PropTypes.string,
     stats: PropTypes.shape({
-      followers: PropTypes.number.isRequired,
-      views: PropTypes.number.isRequired,
-      likes: PropTypes.number.isRequired,
+      followers: PropTypes.number,
+      views: PropTypes.number,
+      likes: PropTypes.number,
     }),
   }),
 };
