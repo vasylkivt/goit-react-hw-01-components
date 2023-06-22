4a9eeb1b7bee5ca3479380f94d5c733ef81583b6
Taras <117749635+vasylkivt@users.noreply.github.com>
2023-06-22 12:29:15 +0300

    fix

--- src/components/userProfile/userProfile.js
+++ src/components/userProfile/userProfile.js
@@ -37,11 +37,15 @@ export default function Profile({
 }
 
 Profile.propTypes = {
-  username: PropTypes.string.isRequired,
-  tag: PropTypes.string.isRequired,
-  location: PropTypes.string.isRequired,
-  avatar: PropTypes.string.isRequired,
-  followers: PropTypes.number.isRequired,
-  views: PropTypes.number.isRequired,
-  likes: PropTypes.number.isRequired,
+  user: PropTypes.shape({
+    username: PropTypes.string,
+    tag: PropTypes.string.isRequired,
+    location: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    stats: PropTypes.shape({
+      followers: PropTypes.number.isRequired,
+      views: PropTypes.number.isRequired,
+      likes: PropTypes.number.isRequired,
+    }),
+  }),
 };
